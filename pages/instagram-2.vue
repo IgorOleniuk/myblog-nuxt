a341bb89dc7497970f2ba9067f45d1b95530f3b5
IgorOleniuk <igor.oleniuk1@gmail.com>
2020-06-01 19:02:41 +0300

    instagram added

--- pages/instagram-2.vue
+++ pages/instagram-2.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="container">
-    <client-only>
-      <vue-instagram userId="17841404120211033" token="IGQVJVQlJiUTJ3VlV3VW9iNDRuMjUwV3Q1a2l1X2tWOUVnX2lpTU1sQTBaMVpSbm5QMTBYMzlSd25mcE5CcTA0UDdiU0ptcjZAIRDhweWNOTENoSlp5ckdYUFM3QnNwLUd0RkZAjTWJB" :count="3" mediaType="image">
-        <template slot="feeds" slot-scope="props">
-          <li class="fancy-list"> {{ props.feed.link }} </li>
-        </template>
-        <template slot="error" slot-scope="props">
-          <div class="fancy-alert"> {{ props.error.error_message }} </div>
-        </template>
-      </vue-instagram>
-    </client-only>
+
   </div>
 </template>
 
--- pages/instagram.vue
+++ pages/instagram.vue
@@ -5,7 +5,11 @@
       :key="photo.id"
       class="mt-5"
     >
-      <img :src="photo.media_url" style="height: 80%; width: 50%">
+      <img
+        v-if="photo.media_type === 'VIDEO'"
+        :src="photo.thumbnail_url"
+      >
+      <img v-else :src="photo.media_url" style="height: 80%; width: 50%">
     </div>
     <a
       href="https://api.instagram.com/oauth/authorize/?client_id=650119452215599&redirect_uri=https://httpstat.us/200&scope=user_profile,user_media&response_type=code"
@@ -17,14 +21,12 @@
 </template>
 
 <script>
-import axios from "axios";
-// import 'instafeed' from 'instafeed.js';
-
-export default {
+  export default {
   data () {
     return {
       photoIds: [],
-      photos: [],
+      accessToken: '',
+      userId: '',
     }
   },
 
@@ -34,28 +36,18 @@ export default {
 
   methods: {
     getPhotosIds() {
-      this.$axios.get('https://graph.instagram.com/17841404120211033/media?access_token=IGQVJVQlJiUTJ3VlV3VW9iNDRuMjUwV3Q1a2l1X2tWOUVnX2lpTU1sQTBaMVpSbm5QMTBYMzlSd25mcE5CcTA0UDdiU0ptcjZAIRDhweWNOTENoSlp5ckdYUFM3QnNwLUd0RkZAjTWJB&fields=id,timestamp,media_url').then((r) => {
-        this.photoIds = r.data.data;
-      })
-    },
+      this.$axios.get('http://pizzavel:8888/api/send-access-token')
+        .then((res) => {
+          this.accessToken = res.data.long_access_token;
+          this.userId = res.data.user_id;
 
-    getAccessToken() {
-      this.$axios.post('https://api.instagram.com/oauth/access_token', {
-        client_id: 650119452215599,
-        client_secret: 'a8a8ada86be51319edc950f60a787e76',
-        grant_type: 'authorization_code',
-        redirect_uri: 'https://httpstat.us/200',
-        code: 'AQAuO64PWz9RXTQNjYlsTwggJcc3sIGZ_0utp9il3DWyC_4Lm0UgnNcmjdkWVkaekQ5fiKSiFZydsxIVudgjF7YfLU213Ek329_PdUGD-rp6wVZcJ6Wayz-8V5fgO0RTFxeQzKHp36XOUztwtUpvx7KL4VEj7sqNbiFfwgxQO0ELbAEpWVrMJtZzLVNhed0ya-KZZCDyXdh9q0dyx9HdO2JJvhcuL9ahVG62Pykp5IvP5A'
-      }).then((r) => {
-        console.log(r);
-      })
-    }
+          this.$axios.get('https://graph.instagram.com/' + this.userId + '/media?access_token=' + this.accessToken + '&fields=id,media_type,media_url,thumbnail_url&limit=3')
+            .then((r) => {
+              console.log(r);
+            this.photoIds = r.data.data;
+          })
+        })
+    },
   },
 }
 </script>
-
-
-"access_token": "IGQVJXYks1aHVVTHh0LVFHdWJudEZAkSC1mdTZAvcklnYjl1b3Juc3E2SUFaTEJrNER5aXh5RlcwUUJKRGV2eUROTVhYb05YeWhDb2pnajR4bUVlU2lfckV1dTFJcC1CUE15a1B6QXJR",
-"token_type": "bearer",
-"expires_in": 5184000
-"user_id": 17841404120211033
